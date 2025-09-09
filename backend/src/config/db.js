801e4fe83df199ce3857375053a39f9fc1772cdd
Prabhu R <prabhurprabhur063@gmail.com>
2025-09-09 13:16:26 +0530

    add user field

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,9 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,      
-  password: process.env.DB_PASSWORD, 
-  database: process.env.DB_NAME,  
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,         
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT)
 });
 
