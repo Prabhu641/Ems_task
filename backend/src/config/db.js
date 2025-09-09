ca15a36730baac2bdd53b3d3db7a94bf17bd740b
Prabhu R <prabhurprabhur063@gmail.com>
2025-09-09 12:50:20 +0530

    change the port field

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,18 +4,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,      // 127.0.0.1
-  user: process.env.DB_USER,      // ems_user
-  password: process.env.DB_PASSWORD, // Prabhumysql@#007
-  database: process.env.DB_NAME,  // employee_management
+  host: process.env.DB_HOST,      
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME,  
   port: Number(process.env.DB_PORT) || 3306,
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("❌ Database connection failed:", err.message);
+    console.error(" Database connection failed:", err.message);
   } else {
-    console.log("✅ Database connected!");
+    console.log(" Database connected!");
   }
 });
 
