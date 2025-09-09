3bd8d7412ccbd1a0d1c9279cf0ea98f1f77a8dbe
Prabhu R <prabhurprabhur063@gmail.com>
2025-09-09 12:39:43 +0530

    fix the style in Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -6,11 +6,8 @@ import Sidebar from "./Sidebar";
 function Layout() {
   return (
     <div className="flex flex-col h-screen bg-gray-100">
-      {/* Full-width header */}
       <Header />
-
       <div className="flex flex-1">
-        {/* Sidebar below header */}
         <Sidebar />
         <main className="flex-1 overflow-auto p-6 bg-gray-100">
           <Outlet />
